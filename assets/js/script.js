42c5489072d28f981ee1d417874f872f64e93b0e
Darlan Talisson <119646089+dtalisson@users.noreply.github.com>
2024-03-07 21:44:28 -0300

    t
    t

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,19 @@
+
+
 document.addEventListener("DOMContentLoaded", function () {
     const hamburger = document.querySelector(".menu-hamburger");
     const navMenu = document.querySelector(".navegacao ul");
+    const menuItems = document.querySelectorAll(".navegacao ul li a");
 
     hamburger.addEventListener("click", function () {
         hamburger.classList.toggle("active");
         navMenu.classList.toggle("active");
+    });
+
+    menuItems.forEach(function (menuItem) {
+        menuItem.addEventListener("click", function () {
+            hamburger.classList.remove("active");
+            navMenu.classList.remove("active");
+        });
     });
 });
